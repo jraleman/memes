6aa3f19b7fff705dfb10a7880c7ea5383f541c73
jraleman <jraleman@bendiburg.com>
2021-06-24 23:37:29 -0700

    Creates useGallery hook

---
+++ src/hooks/useGallery.js
@@ -0,0 +1,23 @@
+const useGallery = () => {
+    const data = useStaticQuery(graphql`
+      query {
+        allFile(
+          filter: { sourceInstanceName: { eq: "gallery" } }
+        ) {
+          nodes {
+            id
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    `);
+  
+    return data.allFile.nodes.map(node => ({
+      ...node.childImageSharp, // Note that we're spreading the childImageSharp object here
+      id: node.id,
+    }));
+};
