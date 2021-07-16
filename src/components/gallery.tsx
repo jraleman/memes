6b266f57b983e2b6c2223203463c65871ea1b7c7
jraleman <jraleman@bendiburg.com>
2021-07-16 17:32:11 -0500

    Implements Gallery component styles

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -3,27 +3,43 @@ import Img from 'gatsby-image';
 import styled from 'styled-components';
 import useGallery from '../hooks/useGallery';
 
-const Gallery = ({ title = 'gallery' }: IGallery) => {
-    const images = useGallery();
+const Gallery = () => {
+  const images = useGallery();
 
-    return (
-        <Container>
-            <Title>{title}</Title>
-            <ImageWrapper>
-                {images.map(({ id, fluid }) => <Img key={id} fluid={fluid} />)}
-            </ImageWrapper>
-        </Container>
-    );
+  return (
+    <ImageWrapper>
+      {images.map(({ id, fluid }) => <Img key={id} fluid={fluid} />)}
+    </ImageWrapper>
+  );
 };
 
-interface IGallery {
-    title?: string;
-}
+const ImageWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 16vw;
+  grid-gap: 0.5em;
 
-const Container = styled.div``;
-
-const Title = styled.h2``;
-
-const ImageWrapper = styled.div``;
+  > * {
+    box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2), 0 3px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+  
+  > :hover {
+    filter: blur(4px);
+    transition: all ease 0.5s;
+    cursor: pointer;
+  }
+  
+  > :nth-child(6n + 3) {
+    grid-column: span 1;
+    grid-row: span 2;
+  }
+  
+  > :nth-child(6n + 2),
+  > :nth-child(6n + 5),
+  > :nth-child(6n + 6) {
+    grid-column: span 2;
+    grid-row: span 2;
+  }
+`;
 
 export default Gallery;
