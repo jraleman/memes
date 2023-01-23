a09e6a1ee2cc41ce6a56a1fed0d961619f68e877
jraleman <jraleman@bendiburg.com>
2023-01-22 21:29:15 -0800

    feat: implements nsfw toggle

--- src/hooks/useGallery.ts
+++ src/hooks/useGallery.ts
@@ -1,16 +1,27 @@
 import { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
+// TODO: Move to constants file(?)
+const nsfwTag = 'NSFW';
+
+// TODO: Move to helpers(?)
+const parseImages = (node: any) => ({ ...node.childImageSharp, id: node.id });
+const randomSorting = () => 0.5 - Math.random();
+const includesNSFW = ({ fluid: { src } }: { fluid: { src: string }}) => src.includes(nsfwTag);
+const excludesNSFW = ({ fluid: { src } }: { fluid: { src: string }}) => !src.includes(nsfwTag);
+
 const useGallery = ({ randomSort }) => {
   const [images, setImages] = useState([]);
   const data = useStaticQuery(query);
-  
-  useEffect(() => {
-    const parseImages = (node) => ({ ...node.childImageSharp, id: node.id });
-    const randomSorting = () => 0.5 - Math.random();
 
-    const arr = data.allFile.nodes.map(parseImages);
-    const imgs = !randomSort ? arr : arr.sort(randomSorting);
+  useEffect(() => {
+    const allImgs = data.allFile.nodes.map(parseImages);
+    const isNsfw = document.location.hash?.split('#')[1]?.toLowerCase() === nsfwTag.toLowerCase(); 
+    let imgs = allImgs.filter(isNsfw ? includesNSFW : excludesNSFW);
+  
+    if (randomSort) {
+      imgs = imgs.sort(randomSorting);
+    }
     setImages(imgs)
   }, [data]);
 
